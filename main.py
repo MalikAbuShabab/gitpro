4a1bf9e6fcbe9c551d356784d746a9ef830bf888
Malik Abo Shabab <malikabushabab@gmail.com>
2024-03-05 19:47:26 +0200

    Add function to sort an array using selection algorithms

--- main.py
+++ main.py
@@ -13,3 +13,13 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 # Swap the elements if they are in the wrong order
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
