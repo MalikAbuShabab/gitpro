d42a121661364320fa1c143a95faf45c0dfc5219
Malik Abo Shabab <malikabushabab@gmail.com>
2024-03-05 19:29:52 +0200

    Add function to sort an array using various sorting algorithms

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ def sum(x,y):
     return r
 
 
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n - 1):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                # Swap the elements if they are in the wrong order
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
